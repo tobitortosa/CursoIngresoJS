418dfa6a983dc16353bd7534818b77d035a5ac2a
Tobi Tortosa <tobiastortosa17@gmail.com>
2023-02-03 14:14:23 -0300

    new exercises added

--- 1-EntradaSalida/jsEntradaSalida-09.js
+++ 1-EntradaSalida/jsEntradaSalida-09.js
@@ -3,8 +3,14 @@ Debemos lograr tomar el importe por ID ,
 transformarlo a entero (parseInt), luego
 mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
-function mostrarAumento()
-{
+function mostrarAumento() {
+  let sueldo = document.getElementById("txtIdSueldo").value;
+  let resultado = document.getElementById("txtIdResultado");
 
-	alert("ok");
+  sueldo = parseInt(sueldo);
+  let sueldoConAumento = Math.floor(sueldo * 1.1);
+
+  resultado.value = sueldoConAumento;
 }
+
+// Tobias Valentin Tortosa
--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -3,7 +3,11 @@ Debemos lograr tomar el importe por ID.
 Transformarlo a entero (parseInt), luego
 mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
-function mostrarAumento()
-{
-	alert("ok");
+function mostrarAumento() {
+  let importe = document.getElementById("txtIdImporte").value;
+  let resultado = document.getElementById("txtIdResultado");
+
+  importe = parseInt(importe);
+  let importeConDescuento = Math.floor(importe - (importe * 1.25 - importe));
+  resultado.value = importeConDescuento;
 }
